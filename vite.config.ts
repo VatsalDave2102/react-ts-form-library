946ca49e7b787d90e25eecda7898b923177bc819
Vatsal Dave <vatsaldave2002@gmail.com>
2024-09-20 15:21:57 +0530

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      staticImport: true,
+      outDir: "dist/types",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "evoke-ui",
+      formats: ["es", "cjs"],
+      fileName: (format) => `${format}/index.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+  },
+});
